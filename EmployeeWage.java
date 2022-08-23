92515d029628d16bd34766ed3df7cf9db9124742
mukeshkapse <mukeshkapse1832@gmail.com>
2022-08-24 01:03:48 +0530

    Added: Employee Is Prasnet or Absent

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,4 +4,17 @@ public class EmployeeWage {
     public static  void main(String[] args)
     {
         System.out.println("Welcome to Employee wage");
-}}
+
+        double Empcheck= Math.floor(Math.random() *10)%2;
+
+        if(Empcheck==1)
+        {
+            System.out.println("Employee is Present");
+
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
+        }
+
+    }}
